20eb70c1eaacc711f2d38d095e58551040573ca5
Chris Aquino <chris.aquino@gmail.com>
2019-03-28 13:33:28 -0400

    runs different code for GET and POST for /users

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const User = require('./models/user');
 // "helper function" === "middleware"
 // a.k.a. "request handler"
 const server = http.createServer(async (req, res) => {
-    console.log(req.url);
+    console.log(req);
 
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
@@ -25,25 +25,31 @@ const server = http.createServer(async (req, res) => {
         res.end(restaurantJSON);
     } else if (req.url.startsWith("/users")) {   
 
+        
         const parts = req.url.split("/");
         console.log(parts);
         // when the req.url is "/users", parts is [ '', 'users' ]
         // when req.url is "/users/3", parts is [ '', 'users', '3' ]
-
-        if (parts.length === 2) {
-            const allUsers = await User.getAll();
-            const userJSON = JSON.stringify(allUsers);    
-            res.end(userJSON);
-        } else if (parts.length === 3) {
-            // the id will be parts[2]
-            const userId = parts[2];
-            // get user by id
-            const theUser = await User.getById(userId);
-            const userJSON = JSON.stringify(theUser);
-            res.end(userJSON);
-        } else {
-            res.statusCode = 404;
-            res.end('Resource not found.');
+        
+        const method = req.method;
+        if (method === "GET") {
+            if (parts.length === 2) {
+                const allUsers = await User.getAll();
+                const userJSON = JSON.stringify(allUsers);    
+                res.end(userJSON);
+            } else if (parts.length === 3) {
+                // the id will be parts[2]
+                const userId = parts[2];
+                // get user by id
+                const theUser = await User.getById(userId);
+                const userJSON = JSON.stringify(theUser);
+                res.end(userJSON);
+            } else {
+                res.statusCode = 404;
+                res.end('Resource not found.');
+            }
+        } else if (method === "POST") {
+            res.end('{ message: "no soup for you"}');
         }
 
     } else {
