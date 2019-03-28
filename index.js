fa125717a9cc6ef204b611815211b30149ef1961
Chris Aquino <chris.aquino@gmail.com>
2019-03-28 14:45:37 -0400

    handles and parses POST body for /users

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const querystring = require('querystring');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -49,11 +50,27 @@ const server = http.createServer(async (req, res) => {
                 res.end('Resource not found.');
             }
         } else if (method === "POST") {
-            res.end('{ message: "it sounds like you would like create"}');
+            // let's read those chunks!
+            let body = '';
+            req.on('data', (chunk) => {
+                // .toString() is built into most objects
+                // it returns a string representation of the object
+                body += chunk.toString();
+            });
+
+            req.on('end', () => {
+                const parsedBody = querystring.parse(body);
+                console.log('====================');
+                console.log(parsedBody);
+                console.log('^^^^^^ BODY OF FORM ^^^^^^^^');
+                res.end('{ "message": "it sounds like you would like create"}');
+            });
+
+
         } else if (method === "PUT") {
-            res.end('{ message: "you wanna update, doncha?"}');
+            res.end('{ "message": "you wanna update, doncha?"}');
         } else if (method === "DELETE") {
-            res.end('{ message: "rm the user"}');
+            res.end('{ "message": "rm the user"}');
         }
 
     } else {
