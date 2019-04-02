e355352666f70fc409067c557067eef3699554e1
Sean R Reid <sean@torchcodelab.com>
2019-04-02 07:25:36 -0400

    * Added simple regex for ID lookup limiting

--- routes/users/index.js
+++ routes/users/index.js
@@ -2,7 +2,14 @@ const users = require('express').Router();
 const all = require('./all');
 const single = require('./single')
 
+// Get a list of all users
 users.get('/', all);
-users.get('/:id', single)
+
+// Get a single user, based on their ID
+users.get('/:id', single);
+
+// Get a single user, but...
+// limit the routes to only single digit searches for IDs 1-9
+// users.get('/:id([1-9]{1})', single);
 
 module.exports = users;
